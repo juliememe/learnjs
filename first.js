dcec18d3e903a08799a6b0bae6641ff877a8ef13
Iuliia Mazaeva <julie_meme@mail.ru>
2022-03-23 12:44:03 +0400

    feat: use strict

--- first.js
+++ first.js
@@ -1,1 +1,7 @@
-alert('First external script!');
+"use strict";
+a = 54;
+let b = 10;
+console.log(b, a);
+
+//Uncaught ReferenceError: a is not defined
+//     at first.js:2:3
--- second.js
+++ second.js
@@ -1,1 +1,3 @@
-alert('Second external script!');
+c = 54;
+let d = 10;
+console.log(c, d);
